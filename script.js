49a34a5086dee8f00e7f74789f68fda873e7b95a
erfan-abm <e.abomardani@gmail.com>
2025-03-18 15:08:12 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      loop:true,
+      margin:20,        
+      nav:true,        
+      autoplay:true,  
+      autoplayHoverPause: true,
+      autoplayTimeout:4000, 
+      responsive:{
+        0:{ items:1 },  
+        600:{ items:2 },
+        1000:{ items:2 } 
+      }
+    });
+  });

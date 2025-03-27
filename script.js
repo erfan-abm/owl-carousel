7b6287f4cee315b257245644f0c42789cc558f05
erfan-abm <e.abomardani@gmail.com>
2025-03-27 15:41:36 +0330

    change style

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
       loop:true,
       margin:20,        
-      nav:true,        
+      nav:false, 
+      dots: true,       
       autoplay:true,  
       autoplayHoverPause: true,
       autoplayTimeout:4000, 
       responsive:{
         0:{ items:1 },  
-        600:{ items:2 },
-        1000:{ items:2 } 
+        600:{ items:1 },
+        1000:{ items:1 } 
       }
     });
   });
+  
